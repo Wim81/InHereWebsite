1434acfae540210f47e4325e4b9679f77c0398b1
Wim Verpoorten <wim.verpoorten@hotmail.com>
2018-09-07 18:54:21 +0200

    Gulp file slightly modified (files in components map were not being watched yet, and now they are)

--- wp-content/themes/inhere_theme01/gulpfile.js
+++ wp-content/themes/inhere_theme01/gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 //paths
-var sassFiles = 'scss/*.scss',
+var sassFiles = 'scss/**/*.scss',
     cssDest = '';
 
 gulp.task('sass', function(){
