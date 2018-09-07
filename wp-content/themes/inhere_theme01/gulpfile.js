bef64d3067c712754d5c5567b790810042939cce
Wim Verpoorten <wim.verpoorten@hotmail.com>
2018-09-07 18:37:12 +0200

    Gulp initiated & setup done for compiling sass (incl. watcher) / Sass setup slightly extended with first partials

---
+++ wp-content/themes/inhere_theme01/gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+//paths
+var sassFiles = 'scss/*.scss',
+    cssDest = '';
+
+gulp.task('sass', function(){
+    gulp.src(sassFiles)
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest(cssDest));
+});
+
+gulp.task('watch',function() {
+    gulp.watch(sassFiles,['sass']);
+});
+
+// Default task
+gulp.task('default', function () {
+    gulp.start('sass');
+    gulp.start('watch');
+});
