f54d2bd62b4208b6ec3f3d1ddcf146358a1a6bb0
Wim Verpoorten <wim.verpoorten@hotmail.com>
2018-09-08 17:06:58 +0200

    Added bootstrap sass files, included this in gulp so it gets compiled automatically, included bootstrap css file in index before my own css and deleted reset in my css (because otherwise that would take away the bootstrap css obviously)

--- wp-content/themes/inhere_theme01/gulpfile.js
+++ wp-content/themes/inhere_theme01/gulpfile.js
@@ -2,10 +2,15 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
 
 //paths
-var sassFiles = 'scss/**/*.scss',
-    cssDest = '';
+var sassFiles = 'scss/**/*.scss';
+var cssDest = '';
+var bootstrapFiles = "bootstrap-4.1.3/scss/**/*.scss"
+var bootstrapCSS = '';
 
 gulp.task('sass', function(){
     gulp.src(sassFiles)
@@ -13,12 +18,20 @@ gulp.task('sass', function(){
         .pipe(gulp.dest(cssDest));
 });
 
+gulp.task('bootstrap', function() {
+    gulp.src(bootstrapFiles)
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest(bootstrapCSS));
+});
+
 gulp.task('watch',function() {
     gulp.watch(sassFiles,['sass']);
+    gulp.watch(bootstrapFiles,['bootstrap']);
 });
 
 // Default task
 gulp.task('default', function () {
     gulp.start('sass');
+    gulp.start('bootstrap');
     gulp.start('watch');
 });
