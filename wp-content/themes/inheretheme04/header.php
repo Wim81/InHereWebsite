1ea3f8978c92c1e66a06660b7ad411cb2bc51027
Wim Verpoorten <wim.verpoorten@hotmail.com>
2019-01-30 21:12:47 +0100

    header now done (with max-width, home & other pages, mobile & desktop)

--- wp-content/themes/inheretheme04/header.php
+++ wp-content/themes/inheretheme04/header.php
@@ -14,19 +14,19 @@
 <div id="page">
 
     <header id="masthead" class="site-header" role="banner">
-
-        <a href="<?php echo get_site_url(); ?>/index.php" class="header-logo-link">
-            <img src="<?php echo get_bloginfo('template_url'); ?>/img/inhere_header_logo.png" alt="site-header-logo" class="header-logo">
-        </a>
-
-        <nav id="site-navigation" class="main-navigation c-nav--top" role="navigation">
-            <?php
-            $args = [
-                'theme_location'        =>  'top-nav'
-            ];
-            wp_nav_menu( $args );
-            ?>
-        </nav>
+        <div class="container-max">
+            <a href="<?php echo get_site_url(); ?>/index.php" class="header-logo-link">
+                <img src="<?php echo get_bloginfo('template_url'); ?>/img/inhere_header_logo.png" alt="site-header-logo" class="header-logo">
+            </a>
+            <nav id="site-navigation" class="main-navigation c-nav--top" role="navigation">
+                <?php
+                $args = [
+                    'theme_location'        =>  'top-nav'
+                ];
+                wp_nav_menu( $args );
+                ?>
+            </nav>
+        </div>
     </header>
 
     <div id="content" class="site-content">
