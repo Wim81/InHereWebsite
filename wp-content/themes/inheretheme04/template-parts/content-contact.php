7f736eda34ce3f314a340a38732289361250bfa2
Wim Verpoorten <wim.verpoorten@hotmail.com>
2019-02-02 22:11:49 +0100

    contact form init

--- wp-content/themes/inheretheme04/template-parts/content-contact.php
+++ wp-content/themes/inheretheme04/template-parts/content-contact.php
@@ -5,8 +5,8 @@
                 <header class="entry-header mb-5">
                     <?php the_title( '<h1>', '</h1>' ); ?>
                 </header>
-                <div class="entry-content mb-5">
-                    <?php the_content(); ?>
+                <div class="entry-content mb-5 form-container">
+                    <?php echo do_shortcode("[contact-form-7 id=\"26\" title=\"Contact form 1\"]"); ?>
                 </div>
             </div>
             <div class="col-md-6">
