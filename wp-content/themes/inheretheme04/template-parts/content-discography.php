512df47d5f2ac8427e5f12e720eb7e6158236910
Wim Verpoorten <wim.verpoorten@hotmail.com>
2019-02-10 16:06:11 +0100

    album detail: tracks layout partially done (flexbox layout done, not yet responsive)

--- wp-content/themes/inheretheme04/template-parts/content-discography.php
+++ wp-content/themes/inheretheme04/template-parts/content-discography.php
@@ -8,7 +8,7 @@
                 </header>
             </div>
         </div>
-        <div class="row mb-5">
+        <div class="row mb-7">
             <div class="col-md-6">
                 <img src="<?php echo get_field('front_image'); ?>" alt="album cover" class="album-detail--image">
             </div>
@@ -18,49 +18,52 @@
                 </p>
             </div>
         </div>
-        <div class="row mb-5">
+
+        <div class="entry-content mb-5">
+
+            <?php $this_album_tracks = get_posts(array(
+                'post_type' => 'track',
+                'meta_query' => array(
+                        array(
+                            'key' => 'album',
+                            'value' => '"' . get_the_ID() . '"',
+                            'compare' => 'LIKE'
+                        )
+                ),
+                'meta_key' =>'rank',
+                'orderby' => 'meta_value_num',
+                'order' => 'ASC',
+                'posts_per_page' => -1
+            )); ?>
+
+            <?php if ( $this_album_tracks ) : ?>
+
+                <div class="all-tracks col-md-10 offset-md-1">
+
+                    <?php foreach  ($this_album_tracks as $track) :
+                    $track_title = get_the_title( $track->ID );
+                    $track_rank = get_post_meta( $track->ID, 'rank', true );
+                    $track_duration = get_post_meta( $track->ID, 'duration', true );
+                    $track_file = get_post_meta( $track->ID, 'file', true );
+                    $track_file_location = wp_get_attachment_url($track_file); ?>
+
+                    <div class="single-track track-<?php echo $track->ID; ?> d-flex flex-row justify-content-between align-items-top">
+                        <audio controls class="single-track--audio">
+                            <source src="<?php echo $track_file_location; ?>">
+                        </audio>
+
+                        <div class="single-track--rank pl-3 pt-3"><?php echo $track_rank; ?></div>
+                        <div class="single-track--title pl-3 pt-3"><?php echo $track_title; ?></div>
+                        <div class="single-track--duration pl-3 pt-3 text-right"><?php echo $track_duration; ?></div>
+                    </div>
+
+                    <?php endforeach; ?>
+
+                </div>;
+
+            <?php endif; ?>
 
         </div>
-
-                <div class="entry-content">
-
-
-                    <?php $this_album_tracks = get_posts(array(
-                       'post_type' => 'track',
-                        'meta_query' => array(
-                                array(
-                                    'key' => 'album',
-                                    'value' => '"' . get_the_ID() . '"',
-                                    'compare' => 'LIKE'
-                                )
-                        ),
-                        'meta_key' =>'rank',
-                        'orderby' => 'meta_value_num',
-                        'order' => 'ASC',
-                    )); ?>
-
-                    <?php if ( $this_album_tracks ) {
-                        echo '<div class="all-tracks">';
-
-                            foreach ( $this_album_tracks as $track ) {
-                            $track_title = get_the_title( $track->ID );
-                            $track_rank = get_post_meta( $track->ID, 'rank', true );
-                            $track_duration = get_post_meta( $track->ID, 'duration', true );
-                            $track_file = get_post_meta( $track->ID, 'file', true );
-                            $track_file_location = wp_get_attachment_url($track_file);
-
-                            echo '<audio controls>';
-                                echo '<source src="' . $track_file_location . '">';
-                            echo'</audio>';
-
-                            echo '<div>' . $track_rank . '</div>';
-                            echo '<div>' . $track_title . '</div>';
-                            echo '<div>' . $track_duration . '</div>';
-                            }
-                        echo '</div>';
-                    } ?>
-
-                </div>
 
                     <a href="<?php bloginfo('url'); ?>/discography/" class="back-to-discography fas fa-angle-double-left">back</a>
             </div>
