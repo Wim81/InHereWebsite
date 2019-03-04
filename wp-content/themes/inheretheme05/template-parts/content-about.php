a511b104729c4a441acbe73eabd1844a413dbd02
Wim Verpoorten <wim.verpoorten@hotmail.com>
2019-03-04 21:53:03 +0100

    some fixes, home gaat ge nog moeten finaal fixen maar doe dat op einde

--- wp-content/themes/inheretheme05/template-parts/content-about.php
+++ wp-content/themes/inheretheme05/template-parts/content-about.php
@@ -1,10 +1,17 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-12">
                 <header class="entry-header mb-5">
                     <?php the_title( '<h1>', '</h1>' ); ?>
                 </header>
+            </div>
+        </div>
+
+
+
+        <div class="row">
+            <div class="col-md-6">
                 <div class="entry-content mb-5">
                     <?php the_content(); ?>
                 </div>
