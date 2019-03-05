376794ea60368933f49360cda261b1655c92d625
Wim Verpoorten <wim.verpoorten@hotmail.com>
2019-03-05 20:42:04 +0100

    small extra fix gradient title & setup about page extended

--- wp-content/themes/inheretheme05/template-parts/content-about.php
+++ wp-content/themes/inheretheme05/template-parts/content-about.php
@@ -8,21 +8,53 @@
             </div>
         </div>
 
-
-
         <div class="row">
             <div class="col-md-6">
                 <div class="entry-content mb-5">
-                    <?php the_content(); ?>
+                    <?php $text_about1 = get_field('text1') ?>
+                    <p><?php echo $text_about1 ?></p>
                 </div>
             </div>
             <div class="col-md-6">
                 <?php
-                $image_about = get_field('main_image');
-                $image_about_url = $image_about['url'];
+                $image_about1 = get_field('image1');
+                $image_about1_url = $image_about1['url'];
                 ?>
-                <img src="<?php echo $image_about_url ?>" alt="inhere-about-image" class="w-100 mb-12 mb-md-10">
+                <img src="<?php echo $image_about1_url ?>" alt="inhere-about-image" class="w-100 mb-12 mb-md-10">
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-md-6">
+                <?php
+                $image_about2 = get_field('image2');
+                $image_about2_url = $image_about2['url'];
+                ?>
+                <img src="<?php echo $image_about2_url ?>" alt="inhere-about-image" class="w-100 mb-12 mb-md-10">
+            </div>
+            <div class="col-md-6">
+                <div class="entry-content mb-5">
+                    <?php $text_about1 = get_field('text1') ?>
+                    <p><?php echo $text_about1 ?></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="entry-content mb-5">
+                    <?php $text_about3 = get_field('text3') ?>
+                    <p><?php echo $text_about3 ?></p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <?php
+                $image_about3 = get_field('image3');
+                $image_about3_url = $image_about3['url'];
+                ?>
+                <img src="<?php echo $image_about3_url ?>" alt="inhere-about-image" class="w-100 mb-12 mb-md-10">
+            </div>
+        </div>
+
     </div>
 </article>
