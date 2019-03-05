dce88d9af52964a33ae8035c63af26139983d109
Wim Verpoorten <wim.verpoorten@hotmail.com>
2019-03-05 21:37:12 +0100

    about page styling desktop done

--- wp-content/themes/inheretheme05/template-parts/content-about.php
+++ wp-content/themes/inheretheme05/template-parts/content-about.php
@@ -8,53 +8,54 @@
             </div>
         </div>
 
-        <div class="row content-box">
-            <div class="col-md-6">
-                <div class="entry-content">
-                    <?php $text_about1 = get_field('text1') ?>
-                    <p><?php echo $text_about1 ?></p>
+        <div class="row content-box content-box-about">
+            <div class="col-12">
+                <div class="about-img about-img-right">
+                    <?php
+                    $image_about1 = get_field('image1');
+                    $image_about1_url = $image_about1['url'];
+                    ?>
+                    <img src="<?php echo $image_about1_url ?>" alt="inhere-about-image" class="w-100">
                 </div>
-            </div>
-            <div class="col-md-6">
-                <?php
-                $image_about1 = get_field('image1');
-                $image_about1_url = $image_about1['url'];
-                ?>
-                <img src="<?php echo $image_about1_url ?>" alt="inhere-about-image" class="w-100">
-            </div>
-        </div>
-
-        <div class="row content-box">
-            <div class="col-md-6">
-                <?php
-                $image_about2 = get_field('image2');
-                $image_about2_url = $image_about2['url'];
-                ?>
-                <img src="<?php echo $image_about2_url ?>" alt="inhere-about-image" class="w-100">
-            </div>
-            <div class="col-md-6">
-                <div class="entry-content">
+                <div class="about-text">
                     <?php $text_about1 = get_field('text1') ?>
                     <p><?php echo $text_about1 ?></p>
                 </div>
             </div>
         </div>
 
-        <div class="row content-box">
-            <div class="col-md-6">
-                <div class="entry-content">
+        <div class="row content-box content-box-about">
+            <div class="col-12">
+                <div class="about-img about-img-left">
+                    <?php
+                    $image_about2 = get_field('image2');
+                    $image_about2_url = $image_about2['url'];
+                    ?>
+                    <img src="<?php echo $image_about2_url ?>" alt="inhere-about-image" class="w-100">
+                </div>
+                <div class="about-text">
+                    <?php $text_about2 = get_field('text2') ?>
+                    <p><?php echo $text_about2 ?></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row content-box content-box-about">
+            <div class="col-12">
+                <div class="about-img about-img-right">
+                    <?php
+                    $image_about3 = get_field('image3');
+                    $image_about3_url = $image_about3['url'];
+                    ?>
+                    <img src="<?php echo $image_about3_url ?>" alt="inhere-about-image" class="w-100">
+                </div>
+                <div class="about-text">
                     <?php $text_about3 = get_field('text3') ?>
                     <p><?php echo $text_about3 ?></p>
                 </div>
             </div>
-            <div class="col-md-6">
-                <?php
-                $image_about3 = get_field('image3');
-                $image_about3_url = $image_about3['url'];
-                ?>
-                <img src="<?php echo $image_about3_url ?>" alt="inhere-about-image" class="w-100">
-            </div>
         </div>
+
 
     </div>
 </article>
