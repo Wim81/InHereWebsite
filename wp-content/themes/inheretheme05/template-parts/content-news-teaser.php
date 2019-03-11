446545d1b666dc29c0adc0463a6616dd15430c81
Wim Verpoorten <wim.verpoorten@hotmail.com>
2019-03-11 20:28:23 +0100

    news teaser background transition ok

--- wp-content/themes/inheretheme05/template-parts/content-news-teaser.php
+++ wp-content/themes/inheretheme05/template-parts/content-news-teaser.php
@@ -1,11 +1,12 @@
 <div class="col-12 news-teaser">
     <a href="<?php the_permalink() ?>" class="link-news-item">
         <article id="post-<?php the_ID(); ?>" <?php post_class("content-box content-box-news-teaser"); ?> >
+            <div class="content-box--inner"></div>
             <h2 class="news-title mb-2"><?php the_title(); ?></h2>
 
             <p class="news--byline mb-5">
                 <span class="news--author">by <?php the_author(); ?>, </span>
-            <span class="news--date"><?php the_date('jS F Y'); ?></span>
+                <span class="news--date"><?php the_date('jS F Y'); ?></span>
             </p>
 
             <div class="entry-content">
