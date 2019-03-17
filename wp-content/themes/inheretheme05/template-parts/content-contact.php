9b5a2a06b7f857b27e028e620d91afd0233a4e8b
Wim Verpoorten <wim.verpoorten@hotmail.com>
2019-03-17 15:07:01 +0100

    responsive fixes #2: contact

--- wp-content/themes/inheretheme05/template-parts/content-contact.php
+++ wp-content/themes/inheretheme05/template-parts/content-contact.php
@@ -8,7 +8,7 @@
             </div>
         </div>
         <div class="row content-box content-box-about">
-            <div class="col-12 mb-5">
+            <div class="col-12 mb-4">
                 <p class="about-intro-text">
                     So, you’ve decided you want to leave me a message? Really? Well, you’re in luck, I have included a form here which allows you to do just that! If you wish to receive a response, please stay polite. If you do not wish to receive a response, you can basically do whatever you like. But remember you would be using up energy which you could have used to improve your life. Deep, isn’t it? No it isn’t.
                 </p>
@@ -23,7 +23,7 @@
                 $image_contact = get_field('main_image');
                 $image_contact_url = $image_contact['url'];
                 ?>
-                <img src="<?php echo $image_contact_url ?>" alt="inhere-contact-image" class="w-100 mb-12 mb-md-10">
+                <img src="<?php echo $image_contact_url ?>" alt="inhere-contact-image" class="w-100 mb-3">
             </div>
         </div>
     </div>
